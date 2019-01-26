7d2165ff145f01844a7a95826b5cfe12e31f44c1
Brent Irwin <brent@brentirwin.tv>
2019-01-26 11:58:44 -0600

    Create room that populates with JSON

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import rooms from './rooms.json';
 import './App.css';
+
+let room = "outside";
+
+// class Links extends Component {
+//   render() {
+
+//     const links = rooms[room].links.map( (link) => {
+//       return (
+//         <li>
+//           <a href={link.href}>{link.text}</a>
+//         </li>
+//       );
+//     });
+
+//     return (
+//       <ul>{links}</ul>
+//     );
+//   }
+// }
 
 class App extends Component {
   render() {
+    console.log(rooms[room].links);
+
+    const links = Object.keys(rooms[room].links).map( (key, index) => {
+      let arr = [];
+
+      let link = rooms[room].links[key];
+      let item = (
+        <li key={index}><a href={link.href}>{link.text}</a></li>
+      );
+      arr.push(item);
+      return arr;
+    });
+
+    console.log(links);
+
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="room-name">{rooms[room].name}</h1>
+          <p className="flavor-text">{rooms.outside.description}</p>
+          <ul>{links}</ul>
         </header>
       </div>
     );
