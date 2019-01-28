e7ccce505d648c1ffe4b5fe87f06ae0850348c7f
Brent Irwin <brent@brentirwin.tv>
2019-01-28 05:01:37 -0600

    Implement links between rooms

--- src/App.js
+++ src/App.js
@@ -1,50 +1,32 @@
 import React, { Component } from 'react';
 import rooms from './rooms.json';
 import './App.css';
-
-let room = "outside";
-
-// class Links extends Component {
-//   render() {
-
-//     const links = rooms[room].links.map( (link) => {
-//       return (
-//         <li>
-//           <a href={link.href}>{link.text}</a>
-//         </li>
-//       );
-//     });
-
-//     return (
-//       <ul>{links}</ul>
-//     );
-//   }
-// }
+import { Room } from './room.js';
+import { Links } from './links.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {room: "outside"};
+
+    this.followLink = this.followLink.bind(this);
+  }
+
+  followLink(dest) {
+    this.setState(state => ({
+      room: dest
+    }));
+  }
+
   render() {
-    console.log(rooms[room].links);
 
-    const links = Object.keys(rooms[room].links).map( (key, index) => {
-      let arr = [];
-
-      let link = rooms[room].links[key];
-      let item = (
-        <li key={index}><a href={link.href}>{link.text}</a></li>
-      );
-      arr.push(item);
-      return arr;
-    });
-
-    console.log(links);
-
+    let currentRoom = rooms[this.state.room];
 
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="room-name">{rooms[room].name}</h1>
-          <p className="flavor-text">{rooms.outside.description}</p>
-          <ul>{links}</ul>
+          <Room room={currentRoom} />
+          <Links room={currentRoom} handleClick={this.followLink}/>
         </header>
       </div>
     );
